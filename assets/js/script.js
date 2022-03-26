5604c8c50bc80ba321b93ef179646101d88ebd4b
Krishan Gharu <kslg257@gmail.com>
2022-03-26 17:24:39 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+    
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
